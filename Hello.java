9f0b0e1b2b778b8de28976561a84beb7a192159f
Jose Antonio Castillo Vegas <josecastillovegas@hotmail.com>
2025-05-17 12:54:52 +0200

    Esta es la version en la que se consigue hacer el add con el ide

--- Hello.java
+++ Hello.java
@@ -6,5 +6,7 @@ public class Hello {
         System.out.println("Se ha añadido un nuevo mensaje de prueba");
 
         System.out.println("Me cago en tus muertos");
+
+        System.out.println("Gilipollas, por fin has conseguido hacer cadd con el ide xd");
     }
 }
