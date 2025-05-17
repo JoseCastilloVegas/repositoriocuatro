82cba767fed5581568a2f89f34dcd690ab2e5413
Jose Antonio Castillo Vegas <josecastillovegas@hotmail.com>
2025-05-17 13:36:53 +0200

    Se ha convertido en el rey xddddddddddddddddd

--- Hello.java
+++ Hello.java
@@ -2,5 +2,7 @@ public class Hello {
 
     public static void main(String[] args){
      System.out.println("He borrado todo y voy a conquistar el main mediante una fusion muajajaja");
+
+     System.out.println("Me he convertido en el rey jajajajaja");
     }
 }
