3d65b870c4ac39d9cd581bec430e4fa0280ee63c
Jose Antonio Castillo Vegas <josecastillovegas@hotmail.com>
2025-05-17 13:26:37 +0200

    Ha conquistado todo y se va a fusiona :O

--- Hello.java
+++ Hello.java
@@ -1,12 +1,6 @@
 public class Hello {
 
     public static void main(String[] args){
-        System.out.println("Hola desde repositorio cuatro :D");
-
-        System.out.println("Se ha añadido un nuevo mensaje de prueba");
-
-        System.out.println("Me cago en tus muertos");
-
-        System.out.println("Gilipollas, por fin has conseguido hacer cadd con el ide xd");
+     System.out.println("He borrado todo y voy a conquistar el main mediante una fusion muajajaja");
     }
 }
