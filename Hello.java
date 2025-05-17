8bf6e976e4499e1f67154257b0b5e86bcf189a82
Jose Antonio Castillo Vegas <josecastillovegas@hotmail.com>
2025-05-17 13:39:47 +0200

    La traidora :((((

--- Hello.java
+++ Hello.java
@@ -1,8 +1,8 @@
 public class Hello {
 
     public static void main(String[] args){
-     System.out.println("He borrado todo y voy a conquistar el main mediante una fusion muajajaja");
+     System.out.println("Te he traiciona mi rey :Dj jajajaja");
 
-     System.out.println("Me he convertido en el rey jajajajaja");
+
     }
 }
